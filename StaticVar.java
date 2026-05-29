eef4ae6e282c3ce3b9852366d5da2858653b68b9
Muhammad-Mubashir-001 <mubashirtahir111@gmail.com>
2026-05-29 17:22:02 +0500

    Static,local and instance variables
    Added exercise of Static,local and instance variables.

---
+++ StaticVar.java
@@ -0,0 +1,31 @@
+
+public class StaticVar {
+    
+static int a = 10; // static variable
+int x = 10;   //instance vatriable
+void method(){
+int b = 10; // local variable
+System.out.println(a+ " " +b+" "+x);
+++a;
+++b;
+    
+}
+    void method1(){
+int b = 10; // local variable
+System.out.println(a+ " " +b+ " "+x);
+++a;
+++b;
+++x;        
+}
+public static void main (String [] args)
+{
+    StaticVar ref = new StaticVar ();
+    StaticVar ref1 = new StaticVar ();
+    
+ref.method();
+ref.method();
+ref1.method1();   
+ref1.method1();  
+ref.method();    
+}
+}
