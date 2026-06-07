79267b84291f8809e8cb2d7251c4b9e0eec084a0
Muhammad-Mubashir-001 <mubashirtahir111@gmail.com>
2026-06-07 13:24:36 +0500

    Added array tasks

---
+++ AddEvens.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class AddEvens
+{
+ public static void main(String[] args)
+ {
+ Scanner keyboard = new Scanner(System.in);
+ System.out.print("Enter number:");
+ int num = keyboard.nextInt();
+ int sum = 0;
+ for(int i = 1; i <= num; i++)
+ {
+ if(i % 2 == 0)
+ {
+ sum += i;
+ }
+ System.out.println(i + " " + num + " " + sum);
+ }
+ System.out.println("Sum of evens is " + sum);
+ }}
---
+++ ArrayAvg.java
@@ -0,0 +1,13 @@
+
+public class ArrayAvg {
+    public static void main(String []args){
+        int[]arr= {1,2,3,4,5,6,7,8,9,10};
+        int sum =0;
+        int avg = 0;
+        for(int i = 0;i<arr.length;i++){
+            sum+= i;
+        }
+        avg = sum/2;
+        System.out.println("Average is :"+avg);
+    }
+}
